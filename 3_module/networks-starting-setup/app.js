67eddf99bab636a16cc2d8d79b3e4983a79d7edc
*vivek <vrvivekofficial@gmail.com>
2025-01-31 13:54:28 +0530

    special domain mongob localhost

--- 3_module/networks-starting-setup/app.js
+++ 3_module/networks-starting-setup/app.js
@@ -68,8 +68,8 @@ app.get('/people', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://localhost:27017/swfavorites',
-  { useNewUrlParser: true },
+  'mongodb://host.docker.internal:27017/swfavorites',
+  { useNewUrlParser: true ,useUnifiedTopology: true},
   (err) => {
     if (err) {
       console.log(err);
